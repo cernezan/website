af82abbc2abb95eeeccddb215f3531f659b7f639
Zan Cerne <mail@cernezan.com>
2023-04-25 19:55:12 +0200

    Filename slugify title

--- .tina/__generated__/prebuild/config.js
+++ .tina/__generated__/prebuild/config.js
@@ -1,6 +1,6 @@
-import { defineConfig as e } from "tinacms";
-const t = "main", l = e({
-  branch: t,
+import { defineConfig as i } from "tinacms";
+const o = "main", a = i({
+  branch: o,
   clientId: "'clientIdHIDDEN'",
   token: "'tokenTinaHIDDEN'",
   build: {
@@ -19,6 +19,15 @@ const t = "main", l = e({
         name: "post",
         label: "Posts",
         path: "src/content/blog/",
+        ui: {
+          filename: {
+            readonly: !1,
+            slugify: (e) => {
+              var t;
+              return `${((t = e == null ? void 0 : e.title) == null ? void 0 : t.toLowerCase().replace(/ /g, "-")) || "NO TITLE DEFINED"}`;
+            }
+          }
+        },
         fields: [
           {
             type: "string",
@@ -36,7 +45,12 @@ const t = "main", l = e({
             type: "string",
             name: "category",
             label: "Category",
-            options: ["\u{1F4BB} Tech", "\u{1F604} Personal", "\u{1F4DD} Article", "\u{1F517} Links"],
+            options: [
+              "\u{1F4BB} Tech",
+              "\u{1F604} Personal",
+              "\u{1F4DD} Article",
+              "\u{1F517} Links"
+            ],
             required: !0
           },
           {
@@ -51,5 +65,5 @@ const t = "main", l = e({
   }
 });
 export {
-  l as default
+  a as default
 };
