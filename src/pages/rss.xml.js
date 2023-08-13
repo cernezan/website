49371d2cab47c3ac530df68eecca83a2322662d8
Zan Cerne <mail@cernezan.com>
2023-08-13 13:14:00 +0200

    Multiple improvments

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,26 +1,28 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_DESCRIPTION } from '../consts';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();	
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_DESCRIPTION } from "../consts";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+import { prepareTitle } from "../utils";
+const parser = new MarkdownIt();
 
 export async function get(context) {
-	let blog = await getCollection('blog');
-	blog = blog.filter((post) => !post.data.draft);
-	return rss({
-		title: "Žan Černe's Blog",
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: blog.map((post) => ({
-			title: post.data.title || '',
-			pubDate: post.data.pubDate,
-			description: post.data.description,
-			customData: post.data.customData,
-			content: sanitizeHtml(parser.render(post.body)),
-			// Compute RSS link from post `slug`
-			// This example assumes all posts are rendered as `/blog/[slug]` routes
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+    let blog = await getCollection("blog");
+    blog = blog.filter((post) => !post.data.draft);
+    return rss({
+        stylesheet: "/rss/styles.xsl",
+        title: "Žan Černe's Blog",
+        description: SITE_DESCRIPTION,
+        site: context.site,
+        items: blog.map((post) => ({
+            title: prepareTitle(post.data.title, "", post.body, true, true),
+            pubDate: post.data.pubDate,
+            description: post.body.slice(0, 140) + "...",
+            customData: post.data.customData,
+            content: sanitizeHtml(parser.render(post.body)),
+            // Compute RSS link from post `slug`
+            // This example assumes all posts are rendered as `/blog/[slug]` routes
+            link: `/blog/${post.slug}/`,
+        })),
+    });
 }
