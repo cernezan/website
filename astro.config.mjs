e76a3121c2e5421e8c0a152e4c1c05dc5319d3c4
Zan Cerne <mail@cernezan.com>
2023-08-17 20:57:43 +0200

    Compress html added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -10,18 +10,18 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 
-import AstroPWA from '@vite-pwa/astro'
-
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cernezan.com',
-  integrations: [mdx(), sitemap(), tailwind(), sitemap(), AstroPWA()],
-  markdown: {
-    drafts: false
-  },
-  experimental: {
-    assets: true
-  },
-  hydrateURLs: true,
+    site: "https://cernezan.com",
+    integrations: [mdx(), sitemap(), tailwind(), sitemap(), AstroPWA()],
+    markdown: {
+        drafts: false,
+    },
+    experimental: {
+        assets: true,
+    },
+    hydrateURLs: true,
+    compressHTML: true,
 });
